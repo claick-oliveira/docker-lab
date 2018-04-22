e8d8a401a23bbf2f0b1abd1331c97f7abe51bba1
claick-oliveira <claick.cao@gmail.com>
2018-04-22 14:58:58 -0300

    Fix bootstrap command install docker-compose

--- scripts/bootstratp.sh
+++ scripts/bootstratp.sh
@@ -6,7 +6,8 @@
 ##############################################################################################################
 
 # Install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o docker-compose
+chmod +x docker-compose && sudo mv docker-compose /usr/local/bin/
 
 # Install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
